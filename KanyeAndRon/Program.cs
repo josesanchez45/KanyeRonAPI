fbc2bc5aae620f1f785fbf55e6b24d8d8ff475d0
jose sanchez <jose.sanchez.artist@gmail.com>
2022-09-09 12:03:16 -0700

    initial commit

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,9 @@
+﻿
+var quote = new QuoteGenerator();
+
+for (int i = 0; i < 5; i++)
+{
+    quote.KanyeQuote();
+    Console.WriteLine();
+    quote.RonQuote();
+}
---
+++ KanyeAndRon/QuoteGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+internal class QuoteGenerator
+{
+    public void KanyeQuote()
+    {
+        var kanyeClient = new HttpClient();
+        var kanyeApi = "https://api.kanye.rest";
+
+        var kanyeResponse = kanyeClient.GetStringAsync(kanyeApi).Result;
+        var kanyeObject = JObject.Parse(kanyeResponse);
+        Console.WriteLine($"Kayne: {kanyeObject["quote"]}");
+    }
+    public void RonQuote()
+    {
+        var ronClient = new HttpClient();
+        var ronApi = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+        var ronResponse = ronClient.GetStringAsync(ronApi).Result;
+        var ronObject = JArray.Parse(ronResponse);
+        Console.WriteLine($"Ron: {ronObject[0]} ");
+
+    }
+}
